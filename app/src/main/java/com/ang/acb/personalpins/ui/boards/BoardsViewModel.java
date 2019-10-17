f84f96b5195fa254d487513f0b727f14f406cb43
Angela Aciobanitei <aciobanitei.angela@gmail.com>
2019-10-17 13:46:04 +0100

    Add pin and board view models

--- app/src/main/java/com/ang/acb/personalpins/ui/boards/BoardsViewModel.java
+++ app/src/main/java/com/ang/acb/personalpins/ui/boards/BoardsViewModel.java
@@ -1,6 +1,67 @@
 package com.ang.acb.personalpins.ui.boards;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Transformations;
 import androidx.lifecycle.ViewModel;
 
+import com.ang.acb.personalpins.data.entitiy.Board;
+import com.ang.acb.personalpins.data.entitiy.Pin;
+import com.ang.acb.personalpins.data.repository.BoardRepository;
+import com.ang.acb.personalpins.utils.AbsentLiveData;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+/**
+ * Stores and manages UI-related data in a lifecycle conscious way.
+ *
+ * See: https://medium.com/androiddevelopers/viewmodels-and-livedata-patterns-antipatterns-21efaef74a54
+ * See: https://medium.com/androiddevelopers/livedata-beyond-the-viewmodel-reactive-patterns-using-transformations-and-mediatorlivedata-fda520ba00b7
+ */
 public class BoardsViewModel extends ViewModel {
+
+    private BoardRepository boardRepository;
+    private final MutableLiveData<Long> boardId = new MutableLiveData<>();
+    private LiveData<List<Board>> allBoards;
+    private LiveData<Board> board;
+    private LiveData<List<Pin>> pins;
+
+    @Inject
+    public BoardsViewModel(BoardRepository boardRepository) {
+        this.boardRepository = boardRepository;
+    }
+
+    public void setBoardId(long id) {
+        boardId.setValue(id);
+    }
+
+    public LiveData<List<Board>> getAllBoards() {
+        if (allBoards == null) {
+            allBoards = boardRepository.getAllBoards();
+        }
+        return allBoards;
+    }
+
+    public LiveData<Board> getBoard() {
+        if (board == null) {
+            board = Transformations.switchMap(boardId, id -> {
+                if (id == null) return AbsentLiveData.create();
+                else return boardRepository.getBoardById(id);
+            });
+        }
+        return  board;
+    }
+
+    public LiveData<List<Pin>> getPinsForBoard() {
+        if (pins == null) {
+            pins = Transformations.switchMap(boardId, id -> {
+                if (id == null) return AbsentLiveData.create();
+                else return boardRepository.getPinsForBoard(id);
+            });
+        }
+        return  pins;
+    }
+
 }
---
+++ app/src/main/java/com/ang/acb/personalpins/utils/AbsentLiveData.java
@@ -0,0 +1,20 @@
+package com.ang.acb.personalpins.utils;
+
+import androidx.lifecycle.LiveData;
+
+/**
+ * A LiveData class that has {@code null} value.
+ *
+ * See: See: https://github.com/android/architecture-components-samples/tree/master/GithubBrowserSample
+ */
+public class AbsentLiveData extends LiveData {
+
+    private AbsentLiveData() {
+        postValue(null);
+    }
+
+    public static <T> LiveData<T> create() {
+        //noinspection unchecked
+        return new AbsentLiveData();
+    }
+}
